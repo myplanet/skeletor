5631dd8c35b3342c33210f2f5562375ba0ff9c0f
andremolnar <andre@becircle.com>
2013-01-30 14:58:29 -0500

    Update the code comments and the bash help

--- kickstart.sh
+++ kickstart.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
-# This is an initialization script for the tools required to make everything work
-# @TODO explain this better - but the code should speak for itself
-
+# This is a project kickstarting script
+# This script does the work of initializing rerun (you don't need to know what that is)
+# And then building the site based on the make files found in the project
 
 PATHTOHERE=`pwd`
 
 if [ -z "$1" ] || [ -z "$2" ]; then
-    echo "Usage: tools-init ProjectName DocRoot"
+    echo "Usage: kickstart ProjectName DocRoot"
     echo "Where ProjectName is the name used in your build script files. e.g. build-ProjectName.make"
     echo "Where DocRoot is where you project is built to. e.g. /var/www/projectName/docroot"
 else
