cd62c7f07520141406c6a41877047aa22c4e182f
andremolnar <andre@becircle.com>
2013-01-30 15:28:49 -0500

    Make kickstart aware of the current branch and have it build that

--- kickstart.sh
+++ kickstart.sh
@@ -3,8 +3,10 @@
 # This is a project kickstarting script
 # This script does the work of initializing rerun (you don't need to know what that is)
 # And then building the site based on the make files found in the project
+# It will always build the current branch you are sitting on.
 
 PATHTOHERE=`pwd`
+CURRENTBRANCH=`git rev-parse --abbrev-ref HEAD`
 
 if [ -z "$1" ] || [ -z "$2" ]; then
     echo "Usage: kickstart ProjectName DocRoot"
@@ -15,6 +17,6 @@ else
 
     export PATH=${PATH}:$PATHTOHERE/tmp/scripts/rerun/core
     export RERUN_MODULES=$PATHTOHERE/tmp/scripts/rerun/custom_modules
-    rerun 2ndlevel:build --project $1 --buildfile build-"$1".make --destination $2 --revision master
+    rerun 2ndlevel:build --project $1 --buildfile build-"$1".make --destination $2 --revision $CURRENTBRANCH
 
 fi
