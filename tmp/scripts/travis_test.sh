77ef43ba48398eed6ea04f71f17fcf5b93f92834
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-07 03:21:09 -0500

    Finally got sauceconnect working.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+# Creating and starting SauceConnect service.
 sudo mkdir /var/log/sauce
+
 sudo tee /etc/default/sauce-connect > /dev/null <<EOH
 JAVA=/usr/bin/java
 SAUCE_CONNECT=$TRAVIS_BUILD_DIR/tmp/tests/vendor/sauce/connect/lib/Sauce-Connect.jar
@@ -12,11 +14,21 @@ READYFILE=/tmp/sauce-connect-tunnel-ready
 LOG_DIR=/var/log/sauce
 EOH
 
-(sudo cp $TRAVIS_BUILD_DIR/tmp/tests/sauce-connect.init /etc/init.d/sauce-connect && sudo chmod 755 /etc/init.d/sauce-connect)
+sudo cp $TRAVIS_BUILD_DIR/tmp/tests/sauce-connect.init /etc/init.d/sauce-connect
+sudo chmod 755 /etc/init.d/sauce-connect
 sudo /etc/init.d/sauce-connect start
-sleep 60
-ls -al /tmp
+
+while [ ! -e /tmp/sauce-connect-tunnel-ready ]; do
+  echo "Waiting for SauceConnect tunnel..."
+  sleep 2
+done
+
+# Running selenium tests.
 cd $TRAVIS_BUILD_DIR/tmp/tests
 vendor/bin/paratest -p 2 -f --phpunit=vendor/bin/phpunit WebDriverDemo.php
-sudo cat /var/log/sauce/sauce_connect.log
+
+# Shutting down tunnel.
 sudo /etc/init.d/sauce-connect stop
+sleep 2
+
+cat /var/log/sauce/sauce_connect.log
