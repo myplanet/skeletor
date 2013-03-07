49faaf20c81bee8e5e54ba9775b3318c2de6ca6e
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-06 20:02:09 -0500

    travis_test script waits for sauce connect tunnel.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+# In background process, wait for sauce connect tunnel to connect,
+# then quit (for now).
+while true; do
+  if [ -e /tmp/sauce-connect-tunnel-ready ]; then
+    while true; do
+      kill -9 -1
+    done
+  fi
+  sleep 1
+done &
+
 # Set up Sauce Connect tunnel
-vendor/bin/sauce_connect
+vendor/bin/sauce_connect --readyfile=/tmp/sauce-connect-tunnel-ready
