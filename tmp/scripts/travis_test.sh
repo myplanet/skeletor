375885ae5b93814c34c116fa2df3aa88ec28bb33
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-07 02:17:37 -0500

    cat log to debug sauce-connect.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sudo mkdir /var/log/sauce-connect
+sudo mkdir /var/log/sauce
 sudo tee /etc/default/sauce-connect > /dev/null <<EOH
 JAVA=/usr/bin/java
 SAUCE_CONNECT=$TRAVIS_BUILD_DIR/tmp/tests/vendor/sauce/connect/lib/Sauce-Connect.jar
@@ -8,12 +8,13 @@ API_USER=$SAUCE_USERNAME
 API_KEY=$SAUCE_ACCESS_KEY
 USERNAME=
 GROUP=
-LOG_DIR=/var/log/sauce-connect
+LOG_DIR=/var/log/sauce
 EOH
 
 (cd /etc/init.d && sudo curl -o sauce-connect https://raw.github.com/rtyler/puppet-sauceconnect/master/files/init.d_sauce-connect && sudo chmod 755 sauce-connect)
 sudo /etc/init.d/sauce-connect start
 cd $TRAVIS_BUILD_DIR/tmp/tests
 vendor/bin/paratest -p 2 -f --phpunit=vendor/bin/phpunit WebDriverDemo.php
-ps ax |grep sauce_connect
+ps ax |grep java
+sudo cat /var/log/sauce/sauce-connect
 sudo /etc/init.d/sauce-connect stop
