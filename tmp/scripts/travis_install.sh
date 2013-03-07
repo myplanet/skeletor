bc2c753915ca35df944348a63baa93ec0de1a11a
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-06 21:31:49 -0500

    Moves composer files in tmp/tests/ dir.

--- tmp/scripts/travis_install.sh
+++ tmp/scripts/travis_install.sh
@@ -4,11 +4,14 @@
 cd $TRAVIS_BUILD_DIR
 cat build-*.make | sed "s/develop/$TRAVIS_COMMIT/g" | drush make --prepare-install php://stdin build
 
+# Start serving the site
+php -S localhost:8000 -t $TRAVIS_BUILD_DIR/build &
+
+cd $TRAVIS_BUILD_DIR/tmp/tests
+
 # Install testing dependencies
 composer install
 
 # Prepare Sauce Labs configs
 vendor/bin/sauce_config $SAUCE_USERNAME $SAUCE_ACCESS_KEY
 
-# Start serving the site
-php -S localhost:8000 -t $TRAVIS_BUILD_DIR/build &
--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -4,10 +4,12 @@
 # then quit (for now).
 while true; do
   if [ -e /tmp/sauce-connect-tunnel-ready ]; then
-    vendor/bin/paratest -p 2 -f --phpunit=vendor/bin/phpunit tmp/tests/WebDriverDemo.php
+    cd $TRAVIS_BUILD_DIR/tmp/tests
+    vendor/bin/paratest -p 2 -f --phpunit=vendor/bin/phpunit WebDriverDemo.php
   fi
   sleep 1
 done &
 
 # Set up Sauce Connect tunnel
+cd $TRAVIS_BUILD_DIR/tmp/tests
 vendor/bin/sauce_connect --readyfile=/tmp/sauce-connect-tunnel-ready
