74f07ade48eefe6f0a8c490fcc20270d5eb7affe
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-07 02:46:25 -0500

    Adds readyfile support.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -3,7 +3,7 @@
 sudo mkdir /var/log/sauce
 sudo tee /etc/default/sauce-connect > /dev/null <<EOH
 JAVA=/usr/bin/java
-SAUCE_CONNECT=$TRAVIS_BUILD_DIR/tmp/tests/vendor/sauce/connect/lib/Sauce-Connect.jar
+SAUCE_CONNECT=$TRAVIS_BUILD_DIR/tmp/tests/vendor/sauce/connect/lib/Sauce-Connect.jar --readyfile=/tmp/sauce-connect-tunnel-ready
 API_USER=$SAUCE_USERNAME
 API_KEY=$SAUCE_ACCESS_KEY
 USERNAME=
@@ -14,9 +14,8 @@ EOH
 (cd /etc/init.d && sudo curl -o sauce-connect https://raw.github.com/rtyler/puppet-sauceconnect/master/files/init.d_sauce-connect && sudo chmod 755 sauce-connect)
 sudo /etc/init.d/sauce-connect start
 sleep 60
+ls -al /tmp
 cd $TRAVIS_BUILD_DIR/tmp/tests
 vendor/bin/paratest -p 2 -f --phpunit=vendor/bin/phpunit WebDriverDemo.php
-ps ax |grep java
-sudo ls /var/log/sauce
 sudo cat /var/log/sauce/sauce_connect.log
 sudo /etc/init.d/sauce-connect stop
