ac64e1efbdcb579d9560d0b68bb51e372c3e7f85
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-14 02:31:15 -0400

    Abandons init script in favour of simpler gist.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -1,36 +1,8 @@
 #!/usr/bin/env bash
 
-# Creating and starting SauceConnect service.
-sudo mkdir /var/log/sauce
-
-sudo tee /etc/default/sauce-connect > /dev/null <<EOH
-JAVA=/usr/bin/java
-SAUCE_CONNECT=$TRAVIS_BUILD_DIR/tmp/tests/vendor/sauce/connect/lib/Sauce-Connect.jar
-API_USER=$SAUCE_USERNAME
-API_KEY=$SAUCE_ACCESS_KEY
-USERNAME=
-GROUP=
-READYFILE=/tmp/sauce-connect-tunnel-ready
-LOG_DIR=/var/log/sauce
-EOH
-
-sudo cp $TRAVIS_BUILD_DIR/tmp/tests/sauce-connect.init /etc/init.d/sauce-connect
-sudo chmod 755 /etc/init.d/sauce-connect
-sudo /etc/init.d/sauce-connect start
-
-while [ ! -e /tmp/sauce-connect-tunnel-ready ]; do
-  echo "Waiting for SauceConnect tunnel..."
-  sleep 2
-done
-
 # Start serving the site
 php -S 127.0.0.1:8080 -t $TRAVIS_BUILD_DIR/build &
 
 # Running selenium tests.
 cd $TRAVIS_BUILD_DIR/tmp/tests
 vendor/bin/paratest -p 2 -f --phpunit=vendor/bin/phpunit WebDriverDemo.php
-
-# Shutting down tunnel.
-sudo /etc/init.d/sauce-connect stop
-
-cat /var/log/sauce/sauce_connect.log
