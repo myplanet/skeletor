77438bafc48a3db92a67c2f75405dd6432b887d0
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-14 13:39:20 -0400

    Experimenting with getting java SeInterpreter on VM.

--- tmp/scripts/travis_install.sh
+++ tmp/scripts/travis_install.sh
@@ -8,3 +8,16 @@ cd $TRAVIS_BUILD_DIR/tmp/tests
 
 # Install testing dependencies
 composer install
+
+# Install beta SeInterpreter by downloading containing repo
+INTERPRETER_DIR="/tmp/se-interpreter-$RANDOM"
+REPO_ARCHIVE_URL="https://github.com/sebuilder/se-builder/archive/gh-pages.zip"
+REPO_ARCHIVE_DOWNLOAD="se-builder-gh-pages.zip"
+INTERPRETER_FILENAME="SeInterpreter*.zip"
+
+mkdir -p $INTERPRETER_DIR
+cd $INTERPRETER_DIR
+curl --location $REPO_ARCHIVE_URL > $REPO_ARCHIVE_DOWNLOAD
+unzip $REPO_ARCHIVE_DOWNLOAD
+rm $REPO_ARCHIVE_DOWNLOAD
+unzip se-builder-gh-pages/$INTERPRETER_FILENAME
