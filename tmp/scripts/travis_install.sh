412f519d734f8799006792b54b43f763303d0eaf
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-14 16:49:06 -0400

    Using only node compile that ever worked, even if it takes forever...

--- tmp/scripts/travis_install.sh
+++ tmp/scripts/travis_install.sh
@@ -9,20 +9,12 @@ cd $TRAVIS_BUILD_DIR/tmp/tests
 # Install testing dependencies
 composer install
 
-# Installs npm
-# http://www.macaronicenglish.com/2012/10/install-nodejs-from-linux-binaries.html
-VERSION=0.8.18
-PLATFORM=linux
-ARCH=x86
-
-cd ~
-wget http://nodejs.org/dist/v$VERSION/node-v$VERSION-linux-x86.tar.gz
-tar xzvf node-v$VERSION-linux-x86.tar.gz
-rm node-v$VERSION-linux-x86.tar.gz
-
-sudo ln -s ~/node-v$VERSION-linux-x86/bin/n /bin/node
-sudo ln -s ~/node-v$VERSION-linux-x86/bin/npm /bin/npm
-sudo ln -s ~/node-v$VERSION-linux-x86/bin/node-waf /bin/node-waf
+# Sources uninitialized nvm and installs correct node/npm.
+source ~/.nvm/nvm.sh
+# Lots of output while building npm, so silencing
+echo "Installing npm v0.8.18. This may take several minutes..."
+nvm install v0.8.18 > /dev/null
+nvm use v0.8.18
 
 # Install se-interpreter node app.
-sudo npm install -g se-interpreter
+sudo /home/travis/.nvm/v0.8.18/bin/npm install -g se-interpreter
