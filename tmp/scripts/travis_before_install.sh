081d8d0894cffdbce48a37e711a0292c0185b345
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-15 01:26:47 -0400

    Adds httpie cli tool. Removes default pass. Finds sessionID and sets correct pass state per test.

--- tmp/scripts/travis_before_install.sh
+++ tmp/scripts/travis_before_install.sh
@@ -11,3 +11,11 @@ phpenv rehash
 # Install Composer (PHP package manager)
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
+
+# Install httpie
+sudo add-apt-repository ppa:afcowie/network --yes
+sudo apt-get update \
+  --option Dir::Etc::sourcelist="sources.list.d/afcowie-network-precise.list" \
+  --option Dir::Etc::sourceparts="-" \
+  --option APT::Get::LVist-Cleanup="0"
+apt-get install httpie
--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -7,4 +7,15 @@ php \
 
 # Running selenium tests.
 cd $TRAVIS_BUILD_DIR/tmp/tests
-se-interpreter selenium/interpreter_config.json
+TEST_CMD='se-interpreter selenium/interpreter_config.json'
+
+if "$TEST_CMD"; then
+  PASS_STATE=true
+else
+  PASS_STATE=false
+fi
+
+# Get Saucelabs sessionID, and set pass state.
+JSON_RESPONSE=$(http --body --auth $SAUCE_USERNAME:$SAUCE_ACCESS_KEY GET https://saucelabs.com/rest/v1/$SAUCE_USERNAME/jobs full==true)
+SESSION_ID=$(ruby -r 'json' -e "\$stdout.write JSON.parse('$JSON_RESPONSE').keep_if {|job| job['browser'] == '$SAUCE_BROWSER' && job['build'] == '$TRAVIS_BUILD_NUMBER' }[0]['id']")
+http --auth $SAUCE_USERNAME:$SAUCE_ACCESS_KEY PUT https://saucelabs.com/rest/v1/$SAUCE_USERNAME/jobs/$SESSION_ID passed:=$PASS_STATE
