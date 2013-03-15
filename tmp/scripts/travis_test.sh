e2d6bf9254b0a19a54c733ec4484e8a8f46d0872
Patrick Connolly <patrick@myplanetdigital.com>
2013-03-15 02:46:04 -0400

    Fixing up after_script to work.

--- tmp/scripts/travis_test.sh
+++ tmp/scripts/travis_test.sh
@@ -5,15 +5,11 @@ php \
   --server 127.0.0.1:8080 \
   --docroot $TRAVIS_BUILD_DIR/build &
 
-# Default to failing if incomplete
-export PASS_STATE=false
-
 # Running selenium tests.
 cd $TRAVIS_BUILD_DIR/tmp/tests
 TEST_CMD='se-interpreter selenium/interpreter_config.json'
 
 if $TEST_CMD; then
-  PASS_STATE=true
   exit 0
 else
   exit 1
