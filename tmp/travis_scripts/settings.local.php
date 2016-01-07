b6acfe37b956c57a7a1d8a061ccc872ca3365cc5
Erin Marchak <erin@emarchak.com>
2016-01-07 14:17:57 -0500

    HOTFIX: add db for travis

--- tmp/travis_scripts/settings.local.php
+++ tmp/travis_scripts/settings.local.php
@@ -1,3 +1,15 @@
+<?php
 /**
- * Local settings configuration.
- */
+  * Local settings configuration.
+  */
+
+$databases['default']['default'] = array (
+  'database' => 'ami',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
