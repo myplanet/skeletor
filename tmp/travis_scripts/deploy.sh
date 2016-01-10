9aea7c5fed73748926d088335079fc8efaa39884
Erin Marchak <erin@emarchak.com>
2016-01-10 14:37:09 -0500

    HOTFIX: remove acquia config before deploy

--- tmp/travis_scripts/deploy.sh
+++ tmp/travis_scripts/deploy.sh
@@ -36,12 +36,12 @@ cd $DEPLOY_DEST
 # If tmp/hooks exists, then copy all files to a folder outside docroot.
 if [ -d $INSTALL_PROFILE/tmp/hooks ]; then
   echo "::Adding Acquia Cloud hooks."
-  cp -ar $INSTALL_PROFILE/tmp/hooks $DEPLOY_DEST
+  cp -a $INSTALL_PROFILE/tmp/hooks $DEPLOY_DEST
 fi
 
 if [ -d $INSTALL_PROFILE/tmp/config ]; then
   echo "::Moving config sync dir outside docroot"
-  mv $INSTALL_PROFILE/tmp/config $DEPLOY_DEST
+  cp -a $INSTALL_PROFILE/tmp/config $DEPLOY_DEST
 fi
 
 echo "::Adding new files."
