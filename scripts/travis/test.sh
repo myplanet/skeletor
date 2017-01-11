e4be20a649c7418092f3c1b36d33a1e23126e04f
Erin Marchak <erin@myplanet.com>
2017-01-11 16:48:25 -0500

    Try out phpcs and coder during travis.

--- scripts/travis/test.sh
+++ scripts/travis/test.sh
@@ -2,7 +2,21 @@
 
 set -e
 
-# Insert smoke tests here!
+echo ":: Running code sniffer."
+phpcs --config-set installed_paths ${PROJECT_ROOT}/vendor/drupal/coder/coder_sniffer
+phpcs -i
+
+phpcs --standard=Drupal \
+  --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md \
+  --ignore=contrib \
+ ${PROJECT_ROOT}/docroot/profiles/${PROFILE}
+
+phpcs --standard=DrupalPractice \
+  --extensions=php,module,inc,install,test,profile,theme,css,info,txt,md \
+  --ignore=contrib \
+ ${PROJECT_ROOT}/docroot/profiles/${PROFILE}
+
+echo ":: Testing cache-rebuild and cron"
 cd ${PROJECT_ROOT}/docroot
 drush cache-rebuild
 drush core-cron
