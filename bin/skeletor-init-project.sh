0a23ec6f8836bb4fafeae0989bdafef2fd5b2baa
Laura Johnson <laurajohnson@Laura-Johnson-MBP.local>
2018-07-10 14:11:31 -0400

    copying .travis.yml and .gitignore to drupal-project root

--- bin/skeletor-init-project.sh
+++ bin/skeletor-init-project.sh
@@ -5,3 +5,9 @@
 # Move all files in project-scaffold to the project root
 
 ln -sfn ./web/profiles/contrib/skeletor/project-scaffold/hooks ./hooks
+
+rm ./.gitignore
+\cp ./web/profiles/contrib/skeletor/project-scaffold/.gitignore ./.gitignore
+
+rm ./.gitignore
+\cp ./web/profiles/contrib/skeletor/project-scaffold/.gitignore ./.gitignore
