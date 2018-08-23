77e05504008874e091af67dd7e0f1c951e8509d5
Rafael Silva <invites@rafaelsilva.net>
2018-08-23 15:52:47 -0400

    SKEL-18: Improving travis to play nice with Acquia

--- project-scaffold/travis/deploy.sh
+++ project-scaffold/travis/deploy.sh
@@ -17,7 +17,9 @@ echo "::Deploying"
 git config --global user.email "travis@myplanet.com"
 git config --global user.name  "Travis CI - $ACQUIA_PROJECT"
 
+echo "::Creating dir ${DEPLOY_DEST}"
 mkdir $DEPLOY_DEST
+echo "::Cloning out ${DEPLOY_REPO} into ${DEPLOY_DEST}"
 git clone $DEPLOY_REPO $DEPLOY_DEST
 cd $DEPLOY_DEST
 git checkout $DEPLOY_BRANCH
@@ -25,8 +27,8 @@ git checkout $DEPLOY_BRANCH
 # Create a travis-build branch off of our test commit since we're in a detached state.
 cd $PROJECT_ROOT
 PULL_REQUEST_MESSAGE=$(git log -n 1 --pretty=format:%s $TRAVIS_COMMIT)
-rm -rf .git
-cp -a $PROJECT_ROOT/. $DEPLOY_DEST
+echo "::Latest message ${PULL_REQUEST_MESSAGE}"
+rsync -a $PROJECT_ROOT/ $DEPLOY_DEST --exclude .git --delete
 
 cd $DEPLOY_DEST
 echo "::Adding new files."
