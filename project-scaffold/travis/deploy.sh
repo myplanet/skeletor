f34d74b53336f1fbeb3ba6f708650a096639e00c
Geoff Appleby <geoff.a@myplanet.com>
2019-05-27 16:24:31 -0700

    Create new deploy branch if necessary

--- project-scaffold/travis/deploy.sh
+++ project-scaffold/travis/deploy.sh
@@ -22,7 +22,17 @@ mkdir $DEPLOY_DEST
 echo "::Cloning out ${DEPLOY_REPO} into ${DEPLOY_DEST}"
 git clone $DEPLOY_REPO $DEPLOY_DEST
 cd $DEPLOY_DEST
-git checkout build-${TRAVIS_BRANCH}
+
+if git rev-parse --verify origin/build-${TRAVIS_BRANCH}
+then
+  # Checkout the existing deploy branch.
+  echo "::Checking out existing branch build-${TRAVIS_BRANCH}"
+  git checkout build-${TRAVIS_BRANCH}
+else
+  # Create a new branch with empty history
+  echo "::Creating new branch build-${TRAVIS_BRANCH}"
+  git checkout --orphan build-${TRAVIS_BRANCH}
+fi
 
 # Create a travis-build branch off of our test commit since we're in a detached state.
 cd $PROJECT_ROOT
